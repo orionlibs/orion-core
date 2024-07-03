7c49dc9a001f9a60aa38707c154c7b84ff97cc73
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2024-07-03 17:33:49 +0100

    added an optional embedded exception-related data object

--- src/main/java/io/github/orionlibs/core/abstraction/OrionCheckedException.java
+++ src/main/java/io/github/orionlibs/core/abstraction/OrionCheckedException.java
@@ -3,11 +3,26 @@ package io.github.orionlibs.core.abstraction;
 public class OrionCheckedException extends Exception
 {
     private static final String DefaultErrorMessage = "There was an error.";
+    protected Object exceptionData;
+
+
+    public OrionCheckedException(Object exceptionData)
+    {
+        super(DefaultErrorMessage);
+        this.exceptionData = exceptionData;
+    }
 
 
     public OrionCheckedException(String errorMessage)
     {
         super(errorMessage);
+    }
+
+
+    public OrionCheckedException(Object exceptionData, String errorMessage)
+    {
+        super(errorMessage);
+        this.exceptionData = exceptionData;
     }
 
 
@@ -17,14 +32,40 @@ public class OrionCheckedException extends Exception
     }
 
 
+    public OrionCheckedException(Object exceptionData, String errorMessage, Object... arguments)
+    {
+        this(exceptionData, String.format(errorMessage, arguments));
+    }
+
+
     public OrionCheckedException(Throwable cause, String errorMessage, Object... arguments)
     {
         super(String.format(errorMessage, arguments), cause);
     }
 
 
+    public OrionCheckedException(Object exceptionData, Throwable cause, String errorMessage, Object... arguments)
+    {
+        super(String.format(errorMessage, arguments), cause);
+        this.exceptionData = exceptionData;
+    }
+
+
     public OrionCheckedException(Throwable cause)
     {
         super(DefaultErrorMessage, cause);
+    }
+
+
+    public OrionCheckedException(Object exceptionData, Throwable cause)
+    {
+        super(DefaultErrorMessage, cause);
+        this.exceptionData = exceptionData;
+    }
+
+
+    public Object getExceptionData()
+    {
+        return exceptionData;
     }
 }
--- src/main/java/io/github/orionlibs/core/abstraction/OrionUncheckedException.java
+++ src/main/java/io/github/orionlibs/core/abstraction/OrionUncheckedException.java
@@ -3,11 +3,26 @@ package io.github.orionlibs.core.abstraction;
 public class OrionUncheckedException extends RuntimeException
 {
     private static final String DefaultErrorMessage = "There was an error.";
+    protected Object exceptionData;
+
+
+    public OrionUncheckedException(Object exceptionData)
+    {
+        super(DefaultErrorMessage);
+        this.exceptionData = exceptionData;
+    }
 
 
     public OrionUncheckedException(String errorMessage)
     {
         super(errorMessage);
+    }
+
+
+    public OrionUncheckedException(Object exceptionData, String errorMessage)
+    {
+        super(errorMessage);
+        this.exceptionData = exceptionData;
     }
 
 
@@ -17,14 +32,40 @@ public class OrionUncheckedException extends RuntimeException
     }
 
 
+    public OrionUncheckedException(Object exceptionData, String errorMessage, Object... arguments)
+    {
+        this(exceptionData, String.format(errorMessage, arguments));
+    }
+
+
     public OrionUncheckedException(Throwable cause, String errorMessage, Object... arguments)
     {
         super(String.format(errorMessage, arguments), cause);
     }
 
 
+    public OrionUncheckedException(Object exceptionData, Throwable cause, String errorMessage, Object... arguments)
+    {
+        super(String.format(errorMessage, arguments), cause);
+        this.exceptionData = exceptionData;
+    }
+
+
     public OrionUncheckedException(Throwable cause)
     {
         super(DefaultErrorMessage, cause);
+    }
+
+
+    public OrionUncheckedException(Object exceptionData, Throwable cause)
+    {
+        super(DefaultErrorMessage, cause);
+        this.exceptionData = exceptionData;
+    }
+
+
+    public Object getExceptionData()
+    {
+        return exceptionData;
     }
 }
